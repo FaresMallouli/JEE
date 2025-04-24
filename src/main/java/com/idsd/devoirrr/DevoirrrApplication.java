d73dda5b8eca35c89cf3e101ec028b96f490b308
FaresMallouli <faresmallouli52@gmail.com>
2025-04-24 09:45:18 +0100

    Add initial project structure and microservice code

---
+++ src/main/java/com/idsd/devoirrr/DevoirrrApplication.java
@@ -0,0 +1,13 @@
+package com.idsd.devoirrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoirrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoirrrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idsd/devoirrr/DevoirrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idsd.devoirrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoirrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
